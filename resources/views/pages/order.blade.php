5055961ce3592da2edf9b6a45c19c0614258a925
AndreeaDamian <andreea.damian@5psolutions.eu>
2021-09-10 17:57:16 +0300

    #26; order

--- resources/views/pages/order.blade.php
+++ resources/views/pages/order.blade.php
@@ -8,15 +8,15 @@
             <table style="width: 50%">
                 <tr>
                     <td style="width: 25%;font-weight: bold;">{{ trans('strings.name') }}</td>
-                    <td><?= $order['name'] ?></td>
+                    <td>{{ $order->name }}</td>
                 </tr>
                 <tr>
                     <td style="width: 25%;font-weight: bold;">{{ trans('strings.contactDetails') }}</td>
-                    <td><?= $order['contact_details'] ?></td>
+                    <td>{{ $order->contact_details }}</td>
                 </tr>
                 <tr>
                     <td style="width: 25%;font-weight: bold;">{{ trans('strings.comment') }}</td>
-                    <td><?= $order['comment'] ?></td>
+                    <td>{{ $order->comment }}</td>
                 </tr>
             </table>
         </div>
@@ -37,11 +37,11 @@
                 <tr>
                     <td>{{ $key + 1 }}</td>
                     <td>{{ $product->title }}</td>
-                    <td><img style="height: 100px;display: flex;" src="{{ $product->image_path ? asset( $product->image_path) : asset('images/placeholder.png') }}"></td>
+                    <td><img style="height: 100px;display: flex;" src="{{ $product->image_path ? asset($product->image_path) : asset('images/placeholder.png') }}"></td>
                     <td>{{ $product->description }}</td>
                     <td>{{ $product->price }}</td>
                 </tr>
-                <?php endforeach ?>
+                @endforeach
             </table>
         </div>
     </section>
